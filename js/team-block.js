f0dab346c372a63a62d45101b2319bda1c03d118
epointal <elisabeth.pointal@free.fr>
2019-01-21 10:59:52 +0100

    gutenberg team block in category widgets instead common

--- js/team-block.js
+++ js/team-block.js
@@ -2,7 +2,7 @@
  * Gutenberg block Javascript code
  * @author epointal
  */
-   // var __                = wp.i18n.__; // The __() function for internationalization.
+    var __                = wp.i18n.__; // The __() function for internationalization.
     var createElement     = wp.element.createElement; // The wp.element.createElement() function to create elements.
     var registerBlockType = wp.blocks.registerBlockType; // The registerBlockType() function to register blocks.
     
@@ -23,9 +23,9 @@
     registerBlockType(
 		'aeris-wppl-team/team-block', // Block name. Must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.	
         {
-            title:  'Team block', // Block title. __() function allows for internationalization.
+            title:  __('Team block'), // Block title. __() function allows for internationalization.
             icon: 'groups', // Block icon from Dashicons. https://developer.wordpress.org/resource/dashicons/.
-			category: 'common', // Block category. Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+			category: 'widgets', // Block category. Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
             attributes: {
 				id: {
                     type: 'number'
@@ -63,7 +63,6 @@
       				)					
                ];
             },
-
             // Defines the saved block (an empty string, there is no html to insert).
             save: function( props ) {
 				return '';
